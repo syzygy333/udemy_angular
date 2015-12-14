52ff2b20ce35ae0d2d30dc1fbc5b00ba532c09e9
syzygy333 <brianjonashogue@gmail.com>
2015-12-14 11:29:01 -0500

    lesson 5 factories

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -41,65 +41,6 @@
     $scope.sortBy = 'name';
     $scope.reverse = false;
 
-    $scope.customers = [
-      {
-        id: 1,
-        joined: '1979-06-08',
-        name:'Brian',
-        city:'North Smithfield',
-        orders: [
-          {
-            id: 1,
-            product: 'Shoes',
-            total: 9.9956
-          }
-        ]
-      },
-      {
-        id: 2,
-        joined: '1986-06-07',
-        name:'Kristen',
-        city:'Gardner'
-        orders: [
-          {
-            id: 2,
-            product: 'Baseball',
-            total: 9.995
-          },
-          {
-            id: 3,
-            product: 'Bat',
-            total: 9.995
-          }
-        ]
-      },
-      {
-        id: 3,
-        joined: 1975-09-17,
-        name:'J',
-        city:'North Smithfield'
-        orders: [
-          {
-            id: 4,
-            product: 'Headphones',
-            total: 44.99
-          }
-        ]
-      },
-      {
-        id: 5,
-        joined: 1984-02-22,
-        name:'Beth',
-        city:'Providence',
-        orders: [
-          {
-            id: 5,
-            product: 'Kindle',
-            total: 101.50
-          }
-        ]
-      }
-    ];
     $scope.doSort = function(propName) {
       $scope.sortBy = propName;
       $scope.reverse = !$scope.reverse;
