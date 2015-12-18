a72cbc9ad03ca4aefa1826e7eb084fba72b7c842
syzygy333 <brianjonashogue@gmail.com>
2015-12-17 22:19:38 -0500

    add http service to factory

--- app/services/customersFactory.js
+++ app/services/customersFactory.js
@@ -1,19 +1,17 @@
 (function() {
-  var customersFactory = function() {
-    
+  var customersFactory = function($http) {
+
     var factory = {};
     factory.getCustomers = function() {
-      return customers;
+      return $http.get('/customers');
     };
     factory.getCustomer = function(customerId) {
-      for(var i = 0,len=customers.length;i<len;i++) {
-        if(customers[i].id === parseInt(cusomterId)) {
-          return customers[i];
-        }
-      }
-      return {};
-    }
+      return $http.get('/customers' + customerId);
+    };
     return factory;
   };
+
+  customersFactory.$inject = ['http'];
+
   angular.module('customersApp').factory('customersFactory', customerFactory);
 }());
